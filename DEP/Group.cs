955fe7b9217d4d1e98850afecaa0b60692b73e41
Freerk <freerk92@hotmail.com>
2018-07-19 10:49:21 +0200

    GroupFile Added

---
+++ DEP/Group.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEP
+{
+    class Group
+    {
+        List<Figure> Figures = new List<Figure>();
+        List<Group> Groups = new List<Group>();
+
+        public void AddToGroup(Figure figure)
+        {
+            Figures.Add(figure);
+        }
+
+        public void AddToGroup(Group group)
+        {
+            Groups.Add(group);
+        }
+    }
+}
