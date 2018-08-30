925b9939d87d2404201cc6eec9f5b3c518d4b909
Freerk Cuperus <fcuperus@newnexusmobile.nl>
2018-08-30 11:48:57 +0200

    GroupResize and Move moved to Group class

--- DEP/Form1.cs
+++ DEP/Form1.cs
@@ -148,39 +148,14 @@ namespace DEP
             }
             else if(!figureSelected && SelectedGroup != null)
             {
-
-                var groupFigures = new List<Figure>();
-                    foreach (var item in newFigures)
-                    {
-                        if (item.group.ID.Equals(SelectedGroup.ID))
-                            groupFigures.Add(item);
-                    }
-
-
-                var mainFigure = groupFigures.First();
-                var width = mainFigure.end.X-mainFigure.start.X;
-                var height = mainFigure.end.Y - mainFigure.start.Y;
-                var EndPoint = mainFigure.end;
-                var StartPoint = mainFigure.start;
-
-                var XDifference = 1.0 - ((double)EndPoint.X - (double)e.X)/width;
-                var YDifference = 1.0 - ((double)EndPoint.Y - (double)e.Y)/height;
-
-                if(XDifference < 0 || YDifference < 0)
-                {
-                    MessageBox.Show("Negatief resizen is voor groepen niet ondersteund","Deze actie is niet mogelijk", MessageBoxButtons.OK);
-                    return;
+                try{
+                    newFigures = SelectedGroup.ResizeGroup(newFigures, e);
                 }
-
-
-                foreach (var item in groupFigures)
+                catch(Exception ex)
                 {
-                    var index = newFigures.IndexOf(item);
-                    var resizedItem = resizeFigure(item, XDifference, YDifference);
-                    if (!item.IsMainGroupFigure)
-                        //resizedItem = AddOffsetToResize(mainFigure, resizedItem, XDifference, YDifference);
-                    newFigures[index] = resizedItem;
-                }               
+                    MessageBox.Show("Negatief resizen is voor groepen niet ondersteund", "Deze actie is niet mogelijk", MessageBoxButtons.OK);
+                }
+         
             }
 
             var drawState = new DrawState(newFigures, newGroup);
@@ -233,18 +208,6 @@ namespace DEP
             return item;
         }
 
-        private Figure resizeFigure(Figure item, double xDifference, double yDifference)
-        {
-            var width = item.end.X - item.start.X;
-            var height = (item.end.Y - item.start.Y);
-            var ItemSizeX = Math.Abs(width*xDifference);
-            var ItemSizeY = Math.Abs(height * yDifference);
-            int newX = Math.Abs((int)(item.start.X + ItemSizeX));
-            int newY = Math.Abs((int)(item.start.Y + ItemSizeY));
-            item.end = new Point(newX, newY);
-            return item;
-        }
-
         private void Move_Down(MouseEventArgs e)
         {
             SaveData.Instance.HistoryList.Add(CloneDrawState());
@@ -267,31 +230,7 @@ namespace DEP
             else if(!figureSelected && SelectedGroup != null)
             {
 
-                var groupFigures = new List<Figure>();
-                    foreach (var item in newFigures)
-                    {
-                        if (item.group.ID.Equals(SelectedGroup.ID))
-                            groupFigures.Add(item);
-                    }
-
-                var StartPoint = groupFigures[0].start;
-                var EndPoint = e.Location;
-                var XDifference = StartPoint.X - EndPoint.X;
-                var YDifference = StartPoint.Y - EndPoint.Y;
-
-                foreach (var item in groupFigures)
-                {
-                    foreach (var groupFigure in newFigures)
-                    {
-                        if (groupFigure.Equals(item))
-                        {
-                            groupFigure.start = new Point(groupFigure.start.X - XDifference, groupFigure.start.Y - YDifference);
-                            groupFigure.end = new Point(groupFigure.end.X - XDifference, groupFigure.end.Y - YDifference);
-                            item.start = groupFigure.start;
-                            item.end = groupFigure.end;
-                        }
-                    }
-                }
+                newFigures = SelectedGroup.MoveGroup(newFigures, e);
                 SetSelectedGroup();
                 Color_Group();
             }
